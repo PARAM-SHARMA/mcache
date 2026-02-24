df08224643cc0975cd19e2a783c8c51fe1d0629b
PARAM-SHARMA <param4sharma@gmail.com>
2026-02-24 14:57:52 +0530

    fix: add typed tests

--- tests/test_cache.cpp
+++ tests/test_cache.cpp
@@ -5,12 +5,14 @@ int main() {
   MCache cache;
 
   assert(cache.add_val("a", "int", "1") == true);
-  // assert(cache.add_val("a", "string", 2) == false);
-  // assert(cache.set_val("a", 3) == true);
-  // auto val = cache.get_val("a");
-  // assert(val && *val == 3);
-  // assert(cache.del_val("a") == true);
-  // assert(cache.get_val("a") == std::nullopt);
+  assert(cache.add_val("a", "string", "2") == false);
+  assert(cache.add_val("a", "int", "test") == false);
+  assert(cache.set_val("a", "string", "2") == true);
+
+  auto val = cache.get_val("a");
+  assert(val.has_value() && val->first == "string" && val->second == "2");
+  assert(cache.del_val("a") == true);
+  assert(cache.get_val("a") == std::nullopt);
 
   return 0;
 }
